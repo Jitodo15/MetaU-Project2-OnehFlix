f7294dacde5b09e26ad8bedf2611150d04ca2f4e
Joy Itodo <joyitodo@meta.com>
2024-06-22 13:30:33 -0700

    Added about and contact to footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,9 @@ import React, {useState} from "react";
 function Footer() {
   return (
     <footer>
+        <p>Contact: joyoneh@gmail.com</p>
         <p>© 2024 OnéhFlix</p>
+        <p>About: Meta U project 3</p>
     </footer>
   )
 }
